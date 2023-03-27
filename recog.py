7ede956ca755cfce411e6b75c7e2e6a938d61b51
Danny Kim <imbird0312@gmail.com>
2023-03-27 22:17:25 +0900

    recog

--- recog.py
+++ recog.py
@@ -1,12 +1,37 @@
 import speech_recognition as sr
+import os
+from gtts import gTTS
+from pygame import mixer
+from time import time, sleep
 
-Recognizer = sr.Recognizer() #인스턴스 생성
+mixer.init()
+Recognizer = sr.Recognizer()
 mic = sr.Microphone()
-with mic as source: #안녕~이라고 말하면
-    audio = Recognizer.listen(source)
-try:
-    data = Recognizer.recognize_google(audio, language="ko")
-except:
-    print("이해하지 못했음")
+
+def speak(text, lang="ko", speed=False):
+    tts = gTTS(text=text, lang=lang, slow=speed)
+    tts.save("./tts.mp3")
+    sleep(1)
+    mixer.Channel(0).play(mixer.Sound('tts.mp3'))
+
+while True:
+    with mic as source:
+        audio = Recognizer.listen(source)
+    try:
+        data = Recognizer.recognize_google(audio, language="ko")
+    except:
+        data = ''
+    if '더블비' in data:
+        mixer.Channel(0).play(mixer.Sound('response.mp3'))
+        with mic as source:
+            audio = Recognizer.listen(source)
+        try:
+            data = Recognizer.recognize_google(audio, language="ko")
+            speak(data)
+        except:
+            mixer.Channel(0).play(mixer.Sound('fail.mp3'))
+    while mixer.Channel(0).get_busy():
+        sleep(.1)
+    print(data)
     
-print(data) #안녕 출력
+
